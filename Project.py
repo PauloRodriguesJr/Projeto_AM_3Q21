3777fe3a016f3bca10a4d9d9453b49e91f8305af
Patrick <patrick.oliveira@aluno.ufabc.edu.br>
2021-10-10 04:50:54 -0300

    removendo chave

--- Project.py
+++
@@ -1,9 +0,0 @@
-# Informações globais (como endereços)
-from dataclasses import dataclass
-
-@dataclass
-class Info:
-    
-    ClientId: str = "e5e660d76be34f69b8ed8bb02441a57b"
-    ClientSecret: str = "7aa080c3fdf349eb95f134401251848d"
-    
